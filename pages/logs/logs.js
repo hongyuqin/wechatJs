0d99f280ff760d932390364e325d6628f052b117
nick.hong <nick.hong@klook.com>
2019-07-10 18:04:34 +0800

    跳转 radio选择

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,13 +3,16 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    items: [
+      {name: 'USA', value: '美国'},
+      {name: 'CHN', value: '中国', checked: 'true'},
+      {name: 'BRA', value: '巴西'},
+      {name: 'JPN', value: '日本'},
+      {name: 'ENG', value: '英国'},
+      {name: 'TUR', value: '法国'},
+    ]
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+  radioChange: function(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
   }
 })
